4f46c796856e9a19064cae8ddbd0fc3774db7e58
A Potašenkov <a.potasenkov@voco.ee>
2023-10-25 16:24:36 +0300

    Write data to file

--- index.js
+++ index.js
@@ -8,20 +8,47 @@ const path = require('path')
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
+const readFile = (filename) =>{
+    return new Promise((resolve, reject) =>{
+        fs.readFile(filename, 'utf-8', (err, data) =>{
+            if (err){
+                reject(err);
+                return;
+            }
+            const tasks = data.split("\n");
+            resolve(tasks);
+        } )
+    } )
+} 
 
-app.get('/', (req,res) => {
-    fs.readFile('../tasks','utf-8', (err,data) =>{
-        if (err) {
-            console.error(err);
-            return;
-        } 
-       
-        const tasks = data.split('/n')
-        res.render('index.ejs', {tasks: tasks} )
-    });
+
+
+app.get('/', async (req,res) =>{
+    const tasks = await readFile('../tasks')
+    console.log(tasks)
+    res.render('index.ejs', {tasks} )
+})
+
+
+// For parsing app 
+app.use(express.urlencoded({extended: true}));
+
+
+app.post('/', async (req,res) => {
+    const tasks = await readFile('../tasks');
+    tasks.push(req.body.task);
+    const data = tasks.join('\n');
+    fs.writeFile('../tasks', data, err =>{
+        if (err) return;
+        res.redirect('/')
+    } )
 })
 
 app.listen(3001, () => {
     console.log('Example app is started at http://localhost:3001')
 
 })
+
+
+
+// 4 Lõpus jäid poolelei
