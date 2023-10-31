9d99472d8f523d6aec971fc74d776bde1733ae5f
A Potašenkov <a.potasenkov@voco.ee>
2023-10-31 14:16:13 +0200

    Delete all tasks data from file

--- index.js
+++ index.js
@@ -15,16 +15,29 @@ const readFile = (filename) =>{
                 reject(err);
                 return;
             }
-            const tasks = data.split("\n");
+            //const tasks = data.split("\n");
+            const tasks = JSON.parse(data)
             resolve(tasks);
         } )
     } )
 } 
 
+const writeFile = (filename,data) => {
+    return new Promise((resolve, reject) => {
+        //get data from file
+        fs.writeFile(filename, data ,'utf-8', err =>{
+            if (err) {
+                console.error(err);
+                return;
+            }  
+            resolve(true)
+        });
+    })
+} 
 
 
 app.get('/', async (req,res) =>{
-    const tasks = await readFile('../tasks')
+    const tasks = await readFile('../tasks.json')
     console.log(tasks)
     res.render('index.ejs', {tasks} )
 })
@@ -34,21 +47,83 @@ app.get('/', async (req,res) =>{
 app.use(express.urlencoded({extended: true}));
 
 
-app.post('/', async (req,res) => {
-    const tasks = await readFile('../tasks');
-    tasks.push(req.body.task);
-    const data = tasks.join('\n');
-    fs.writeFile('../tasks', data, err =>{
-        if (err) return;
-        res.redirect('/')
-    } )
+app.post('/', (req,res) => {
+    readFile('../tasks.json')
+        .then(tasks => {
+            
+            let index
+            if(tasks.length === 0){
+                index = 0
+            }  else{
+                index = tasks[tasks.length - 1].id + 1
+            }  
+            const newTask = {
+                "id":index,
+                "task":req.body.task
+            }
+            console.log(newTask)
+            
+            tasks.push(newTask)
+            console.log(tasks)
+            data = JSON.stringify(tasks, null, 2)
+            
+            // Mulle endale
+            // Kirjutasin 5. ülesande 8.lehe koodijupi
+            writeFile('tasks.json', data)
+            //-------------------------
+            console.log(data)
+            fs.writeFile('../tasks.json', data, 'utf8', (err,data) => {
+                if(err){
+                    console.error(err)
+                    return;
+                } else{
+                    console.log('saved')
+                }   
+                res.redirect('/')
+            }) 
+        }) 
+    
 })
 
-app.listen(3001, () => {
+
+app.get('/delete-task/:taskId', (req,res) => {
+    let deletedTaskId = parseInt(req.params.taskId)
+    readFile('../tasks.json')
+    .then(tasks =>{
+        tasks.forEach((task, index) =>{
+            if(task.id === deletedTaskId){
+                tasks.splice(index, 1)
+            } 
+        } )
+        data = JSON.stringify(tasks, null, 2)
+        // Mulle endale
+        // Kirjutasin 5. ülesande 8.lehe koodijupi
+        writeFile('tasks.json', data)
+        //-------------------------
+        fs.writeFile('../tasks.json', data, 'utf-8', err => {
+            if (err){
+                console.error(err);
+                return;
+            }  
+            //redirect to / to see result
+            res.redirect('/')
+        } )
+    } )
+} )
+
+app.post('/clear-tasks', (req,res) =>{
+    fs.writeFile('../tasks.json',JSON.stringify([],null,2),'utf-8',(err) => {
+        if (err){
+            console.log(err);
+            return;
+        } 
+        res.redirect('/')
+    });
+});
+
+app.listen(3000, () => {
     console.log('Example app is started at http://localhost:3001')
 
 })
+// J'tka kuuendal lehel
 
-
-
-// 4 Lõpus jäid poolelei
